1cd2996bfba01b4776ba0d9ba2f5eea65118e2ba
Jared Sutch <sutch002@gmail.com>
2021-06-01 20:02:48 -0500

    updated routes

--- models/index.js
+++ models/index.js
@@ -10,3 +10,29 @@ User.hasMany(Post, {
 Post.belongsTo(User);
 
 module.exports = { User, Post };
+
+/* User.hasMany(Comment, {
+foreignKey: "user_id",
+onDelete: "CASCADE",
+}
+
+Comment.belongsTo(User);
+
+Post.hasMany(Comment, {
+foreignKey: "post_id",
+onDelete: "CASCADE",
+
+})
+Comment.belongsTo(Post);
+
+
+
+
+
+
+
+
+
+
+
+*/
