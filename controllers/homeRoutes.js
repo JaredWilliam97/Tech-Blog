2272fe90c215b2cb3ffd3195c187b5ff2fcdea84
Jared Sutch <sutch002@gmail.com>
2021-06-01 20:29:30 -0500

    updated homeroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,149 @@
 const router = require("express").Router();
+const withAuth = require("../utils/auth");
+const { sequelize } = require("../models/users");
+const { User, Post, Comment } = require("../models");
 
+//base url: http://localhost:8080/ +
+
+// display home page
 router.get("/", async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render("homepage");
+  try {
+    // get the posts (if any) - these display regardless of login status
+    const rawPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name", "created_at", "updated_at"],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+          ],
+          attributes: [
+            "comment",
+            "created_at",
+            "updated_at",
+            [
+              sequelize.fn(
+                "IFNULL",
+                sequelize.col("comments.updated_at"),
+                sequelize.col("comments.created_at")
+              ),
+              "maxDate",
+            ],
+          ],
+          order: ["maxDate", "DESC"],
+        },
+      ],
+    });
+
+    // ensure data was found
+    if (!rawPostData) {
+      res.status(404).json({ message: "No posts found." });
+    }
+
+    // serialize the posts
+    const postData = rawPostData.map((post) => post.get({ plain: true }));
+
+    // console.log(postData);
+
+    // render home page - submit session.loggedIn status for page
+    res.render("homepage", {
+      style: "postDetails.css",
+      posts: postData,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login", {
+    style: "login.css",
+  });
+});
+
+router.get("/createUser", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("createUser", {
+    style: "createUser.css",
+  });
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  // console.log("dashboard");
+  try {
+    const userId = req.session.user_id;
+
+    const rawPostsData = await Post.findAll({
+      where: { user_id: userId },
+      include: [
+        {
+          model: User,
+          attributes: ["name", "created_at", "updated_at"],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+          ],
+          attributes: [
+            "comment",
+            "created_at",
+            "updated_at",
+            [
+              sequelize.fn(
+                "IFNULL",
+                sequelize.col("comments.updated_at"),
+                sequelize.col("comments.created_at")
+              ),
+              "maxDate",
+            ],
+          ],
+          order: ["maxDate", "DESC"],
+        },
+      ],
+    });
+
+    console.log("rawPostsData:", rawPostsData);
+    // if(!rawPostsData) {
+    //   res.status(404).json({message: "No posts found!"})
+    // }
+    const postData = await rawPostsData.map((post) =>
+      post.get({ plain: true })
+    );
+
+    postData.forEach((post) => (post.showEdit = true));
+
+    console.log(postData);
+
+    res.render("dashboard", {
+      style: "postDetails.css",
+      posts: postData,
+      loggedIn: req.session.loggedIn,
+      showEdit: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
