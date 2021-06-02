17e8c7dc8ccb5912b6c7a495332b565182dfd229
Jared Sutch <sutch002@gmail.com>
2021-06-01 23:57:16 -0500

    updated alot folders

--- utils/helper.js
+++ utils/helper.js
@@ -1,6 +1,9 @@
 module.exports = {
   // add helpers here
   format_date: (date) => {
-    return date.toLocaleString();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear() + 5;
+    return `${month}/${day}/${year}`;
   },
 };
